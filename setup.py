015a4f6dbb5d080700d346b632164d5efeaa199a
Pierre-Anthony Lemieux <pal@palemieux.com>
2021-08-07 17:59:02 -0700

    Move to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
   name='imschrm', 
-  version='1.0.0rc1',
+  version='1.0.0',
   description='Validates IMSC documents against the IMSC HRM',
   long_description=long_description,
   long_description_content_type="text/markdown",
