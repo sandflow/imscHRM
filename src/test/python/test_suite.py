1fb0ed10ed9d32f0306e2421ecae19c604c95d7a
Pierre-Anthony Lemieux <pal@palemieux.com>
2023-07-13 08:30:06 -0700

    Fix tests Started adding pass tests

--- src/test/python/test_suite.py
+++ src/test/python/test_suite.py
@@ -45,7 +45,7 @@ class EventHandler(imschrm.hrm.EventHandler):
 
 class HRMTestSuite(unittest.TestCase):
 
-  def test_test_suite(self):
+  def test_fail_test_suite(self):
     ev = EventHandler()
     for root, _subdirs, files in os.walk("src/test/resources/ttml/test-suite/fail"):
       for filename in files:
@@ -55,6 +55,17 @@ class HRMTestSuite(unittest.TestCase):
             with open(os.path.join(root, filename), "r", encoding="utf-8") as f:
               imschrm.hrm.validate(imschrm.doc_sequence.iter_isd([(0, None, f.read())]), ev, 0)
               self.assertTrue(ev.failed)
+
+  def test_pass_test_suite(self):
+    ev = EventHandler()
+    for root, _subdirs, files in os.walk("src/test/resources/ttml/test-suite/pass"):
+      for filename in files:
+        (name, ext) = os.path.splitext(filename)
+        if ext == ".ttml":
+          with self.subTest(name):
+            with open(os.path.join(root, filename), "r", encoding="utf-8") as f:
+              imschrm.hrm.validate(imschrm.doc_sequence.iter_isd([(0, None, f.read())]), ev, 0)
+              self.assertFalse(ev.failed)
 
 if __name__ == '__main__':
   unittest.main()
