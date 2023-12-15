62944e384e836ac91dc6de014a73e7f09224385e
Pierre-Anthony Lemieux <pal@sandflow.com>
2023-12-14 20:46:09 -0800

    Bump version to 1.1.0b7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
   name='imschrm',
-  version='1.1.0b6',
+  version='1.1.0b7',
   description='Validates IMSC documents against the IMSC HRM',
   long_description=long_description,
   long_description_content_type="text/markdown",
