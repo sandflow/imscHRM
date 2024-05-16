0c83fc7f4e05430681cc8b2aed242e80836e78d4
Pierre-Anthony Lemieux <pal@palemieux.com>
2024-05-16 11:17:40 -0700

    Bump to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
   name='imschrm',
-  version='1.1.0-rc.1',
+  version='1.1.0',
   description='Validates IMSC documents against the IMSC HRM',
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setup(
     'Bug Reports': 'https://github.com/sandflow/imscHRM/issues',
     'Source': 'https://github.com/sandflow/imscHRM',
   },
-  install_requires = ["ttconv>=1.1.0rc1"],
+  install_requires = ["ttconv>=1.1.0"],
   classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
